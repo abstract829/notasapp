d1a2600122f4b3267cd556e04374c90bcdc939e2
abstract829 <maxxii.1820@gmail.com>
2021-12-20 18:23:32 -0300

    api-url-netlify

--- services/notas.js
+++ services/notas.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchApi = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://notesapp-next.netlify.app/api",
 });
 
 export const getNotes = async () => {
